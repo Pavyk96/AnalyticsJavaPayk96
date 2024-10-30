479d2b26f4f3e50f00c991321f4d5bc8103bacb2
payk96 <pepethefrog27@mail.ru>
2024-10-30 13:33:13 +0500

    Update model

--- src/main/java/org/example/Model/Course.java
+++ src/main/java/org/example/Model/Course.java
@@ -1,10 +1,16 @@
 package org.example.Model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Setter
+@Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Course {
     private String name;
     private List<Student> students;
